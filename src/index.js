d91d410adcd1e06fd9c579ce9e481bb5e8cf1f0b
Juhye-Kim <kjh960215@gmail.com>
2022-01-10 22:49:07 +0900

    add module 사용

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from "lodash";
+import add from "./add";
 
 function component() {
   const element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["add", "4 and 5 is "], " ");
+  element.innerHTML += add(4, 5);
 
   return element;
 }
